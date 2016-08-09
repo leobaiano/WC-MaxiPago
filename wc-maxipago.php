460c06ce9d322448237e1450f6cfe421b64ec8cf
Leo Baiano <ljunior2005@gmail.com>
2016-08-09 10:46:28 -0300

    adiciona if que verifica se já existe uma classe com mesmo nome para evitar conflitos

--- wc-maxipago.php
+++ wc-maxipago.php
@@ -14,100 +14,102 @@
 	if ( ! defined( 'ABSPATH' ) )
 		exit; // Exit if accessed directly.
 
-	/**
-	 * WC MaxiPago
-	 *
-	 * @author   Leo Baiano <ljunior2005@gmail.com>
-	 */
-	class WC_MaxiPago {
+	if ( ! class_exists( 'WC_MaxiPago' ) ) :
 		/**
-		 * Instance of this class.
+		 * WC MaxiPago
 		 *
-		 * @var object
+		 * @author   Leo Baiano <ljunior2005@gmail.com>
 		 */
-		protected static $instance = null;
+		class WC_MaxiPago {
+			/**
+			 * Instance of this class.
+			 *
+			 * @var object
+			 */
+			protected static $instance = null;
 
-		/**
-		 * Slug.
-		 *
-		 * @var string
-		 */
-		protected static $text_domain = 'wc-maxipago';
+			/**
+			 * Slug.
+			 *
+			 * @var string
+			 */
+			protected static $text_domain = 'wc-maxipago';
 
-		/**
-		 * Initialize the plugin
-		 */
-		private function __construct() {
-			// Load plugin text domain
-			add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
+			/**
+			 * Initialize the plugin
+			 */
+			private function __construct() {
+				// Load plugin text domain
+				add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
 
-			// Load styles and script
-			add_action( 'wp_enqueue_scripts', array( $this, 'load_styles_and_scripts' ) );
-			add_action( 'admin_enqueue_scripts', array( $this, 'load_admin_styles_and_scripts' ) );
+				// Load styles and script
+				add_action( 'wp_enqueue_scripts', array( $this, 'load_styles_and_scripts' ) );
+				add_action( 'admin_enqueue_scripts', array( $this, 'load_admin_styles_and_scripts' ) );
 
-			// Load Helpers
-			add_action( 'init', array( $this, 'load_helper' ) );
-		}
-
-		/**
-		 * Return an instance of this class.
-		 *
-		 * @return object A single instance of this class.
-		 */
-		public static function get_instance() {
-			// If the single instance hasn't been set, set it now.
-			if ( null == self::$instance ) {
-				self::$instance = new self;
+				// Load Helpers
+				add_action( 'init', array( $this, 'load_helper' ) );
 			}
 
-			return self::$instance;
-		}
+			/**
+			 * Return an instance of this class.
+			 *
+			 * @return object A single instance of this class.
+			 */
+			public static function get_instance() {
+				// If the single instance hasn't been set, set it now.
+				if ( null == self::$instance ) {
+					self::$instance = new self;
+				}
 
-		/**
-		 * Load the plugin text domain for translation.
-		 *
-		 * @return void
-		 */
-		public function load_plugin_textdomain() {
-			load_plugin_textdomain( self::$text_domain, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-		}
-
-		/**
-		 * Load styles and scripts
-		 *
-		 */
-		public function load_styles_and_scripts() {
-			wp_enqueue_style( self::$text_domain . '_css_main', plugins_url( '/assets/css/main.css', __FILE__ ), array(), null, 'all' );
-			$params = array(
-						'ajax_url'	=> admin_url( 'admin-ajax.php' )
-					);
-			wp_enqueue_script( self::$text_domain . '_js_main', plugins_url( '/assets/js/main.js', __FILE__ ), array( 'jquery' ), null, true );
-			wp_localize_script( self::$text_domain . '_js_main', 'data_baianada', $params );
-		}
-
-		/**
-		 * Load styles and scripts
-		 *
-		 */
-		public function load_admin_styles_and_scripts() {
-			wp_enqueue_style( self::$text_domain . '_admin_css_main', plugins_url( '/assets/css/admin_main.css', __FILE__ ), array(), null, 'all' );
-			$params = array(
-						'ajax_url'	=> admin_url( 'admin-ajax.php' )
-					);
-			wp_enqueue_script( self::$text_domain . '_admin_js_main', plugins_url( '/assets/js/admin_main.js', __FILE__ ), array( 'jquery' ), null, true );
-			wp_localize_script( self::$text_domain . '_admin_js_main', 'data_baianada', $params );
-		}
-
-		/**
-		 * Load auxiliary and third classes are in the class directory
-		 *
-		 */
-		public function load_helper() {
-			$class_dir = plugin_dir_path( __FILE__ ) . "/helper/";
-			foreach ( glob( $class_dir . "*.php" ) as $filename ){
-				include $filename;
+				return self::$instance;
 			}
-		}
 
-	} // end class Baianada();
-	add_action( 'plugins_loaded', array( 'WC_MaxiPago', 'get_instance' ), 0 );
+			/**
+			 * Load the plugin text domain for translation.
+			 *
+			 * @return void
+			 */
+			public function load_plugin_textdomain() {
+				load_plugin_textdomain( self::$text_domain, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+			}
+
+			/**
+			 * Load styles and scripts
+			 *
+			 */
+			public function load_styles_and_scripts() {
+				wp_enqueue_style( self::$text_domain . '_css_main', plugins_url( '/assets/css/main.css', __FILE__ ), array(), null, 'all' );
+				$params = array(
+							'ajax_url'	=> admin_url( 'admin-ajax.php' )
+						);
+				wp_enqueue_script( self::$text_domain . '_js_main', plugins_url( '/assets/js/main.js', __FILE__ ), array( 'jquery' ), null, true );
+				wp_localize_script( self::$text_domain . '_js_main', 'data_baianada', $params );
+			}
+
+			/**
+			 * Load styles and scripts
+			 *
+			 */
+			public function load_admin_styles_and_scripts() {
+				wp_enqueue_style( self::$text_domain . '_admin_css_main', plugins_url( '/assets/css/admin_main.css', __FILE__ ), array(), null, 'all' );
+				$params = array(
+							'ajax_url'	=> admin_url( 'admin-ajax.php' )
+						);
+				wp_enqueue_script( self::$text_domain . '_admin_js_main', plugins_url( '/assets/js/admin_main.js', __FILE__ ), array( 'jquery' ), null, true );
+				wp_localize_script( self::$text_domain . '_admin_js_main', 'data_baianada', $params );
+			}
+
+			/**
+			 * Load auxiliary and third classes are in the class directory
+			 *
+			 */
+			public function load_helper() {
+				$class_dir = plugin_dir_path( __FILE__ ) . "/helper/";
+				foreach ( glob( $class_dir . "*.php" ) as $filename ){
+					include $filename;
+				}
+			}
+
+		} // end class Baianada();
+		add_action( 'plugins_loaded', array( 'WC_MaxiPago', 'get_instance' ), 0 );
+	endif;

93eef638ff1c8431e6805e445588da978d77cc67
Leo Baiano <ljunior2005@gmail.com>
2016-08-09 11:58:13 -0300

    adiciona maxipago nas opções de checkout do WooCommerce

--- includes/wc-maxipago-gateway.php
+++ includes/wc-maxipago-gateway.php
@@ -12,5 +12,17 @@
 	 * WooCommerce PagSeguro gateway.
 	 */
 	class WC_MaxiPago_Gateway extends WC_Payment_Gateway {
-
+		/**
+		 * Construct Method
+		 *
+		 * @since 1.0.0
+		 */
+		public function __construct() {
+			$this->id = 'maxipago';
+			$this->icon = apply_filters( 'woocommerce_maxipago_icon', plugins_url( 'assets/images/icon-maxipago.png', plugin_dir_path( __FILE__ ) ) );
+			$this->has_fields = false;
+			$this->method_title = __( 'MaxiPago', 'wc-maxipago' );
+			$this->method_description = __( 'Accept payments by credit card, bank debit or banking ticket using the MAxiPago.', 'wc-maxipago' );
+			$this->order_button_text  = __( 'Proceed to payment', 'wc-maxipago' );
+		}
 	}
